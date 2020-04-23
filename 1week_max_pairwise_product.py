5c236b77b61af6f69b74647f39c39b298c9c113d
Ann <ann.matrokhina@gmail.com>
2020-04-23 13:19:19 +0100

    add 1week_exercise2

---
+++ 1week_max_pairwise_product.py
@@ -0,0 +1,26 @@
+# python3
+
+
+def max_pairwise_product(numbers):
+    n = len(numbers)
+    max_product = 0
+    for first in range(n):
+        for second in range(first + 1, n):
+            max_product = max(max_product,
+                              numbers[first] * numbers[second])
+
+    return max_product
+
+
+def max_foo(list):
+    n1 = max(list)
+    list.remove(n1)
+    n2 = max(list)
+    return n1*n2
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    input_numbers = [int(x) for x in input().split()]
+    # print(max_pairwise_product(input_numbers))
+    print(max_foo(input_numbers))
